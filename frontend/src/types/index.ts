b655b3a6c9210223a50bdbbc7f1cac9b6e176696
Daniel Monteiro <>
2025-02-03 16:31:55 +0000

    Add TypeScript interfaces for User and Job models

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,15 @@
+export interface User {
+    id: number;
+    name: string;
+    lastName: string;
+    email: string;
+    role: string;
+    profilePicture: any;
+  }
+  
+  export interface Job {
+    id: number;
+    title: string;
+    description: string;
+  }
+  
